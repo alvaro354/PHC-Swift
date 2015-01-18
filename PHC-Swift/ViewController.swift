c44107ed5aa98fe910473885e4805dbc6d575ad1
alvaro354 <alvaro354@MacBook-Air-de-Alvaro.local>
2015-01-18 19:38:34 +0100

    Initial Commit

---
+++ PHC-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PHC-Swift
+//
+//  Created by Alvaro Lancho  on 18/1/15.
+//  Copyright (c) 2015 LanchoSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
